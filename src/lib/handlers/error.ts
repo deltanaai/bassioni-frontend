f2fbe0ef5101adffd05ce18bc540fba5e2274e97
Khaled Rizk <kh413d413@gmail.com>
2025-10-13 10:45:23 +0200

    Implement Logging

--- src/lib/handlers/error.ts
+++ src/lib/handlers/error.ts
@@ -2,6 +2,7 @@ import { error } from "console";
 import { NextResponse } from "next/server";
 import { RequestError, ValidationError } from "../http-errors";
 import z, { ZodError } from "zod";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
@@ -26,6 +27,11 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} - Error: ${error.message}`
+    );
+
     return formatResponse(
       responseType,
       error.status,
@@ -39,6 +45,11 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
       error.flatten().fieldErrors as Record<string, string[]>
     );
 
+    logger.error(
+      { err: error },
+      `Validation Error: ${validationError.message}`
+    );
+
     return formatResponse(
       responseType,
       validationError.status,
@@ -48,9 +59,11 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
     return formatResponse(responseType, 500, error.message);
   }
 
+  logger.error({ err: error }, "Unknown error occured");
   return formatResponse(responseType, 500, "An unknown error occurred");
 };
 
---
+++ src/lib/logger.ts
@@ -0,0 +1,19 @@
+import pino from "pino";
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      ignore: "pid,hostname",
+      translateTime: "SYS:standard",
+    },
+  },
+  formatters: {
+    level: (label) => ({ level: label.toUpperCase() }),
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+});
+
+export default logger;
